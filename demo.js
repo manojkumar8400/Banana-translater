38072e17ab50079dd3b615dcd3ef62254035bbdc
Manoj Kumar <manojprajapati8400@gmail.com>
2021-11-27 16:53:01 +0530

    initial comment

---
+++ demo.js
@@ -0,0 +1,26 @@
+var btnTranslate = document.querySelector("#Translate-button");
+var textinput = document.querySelector("#Translate-input");
+var outputDiv = document.querySelector("#Translate-output");
+
+var serverURl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURl(serverURL) {
+    return serverURL + "?" + "text=" + textinput.value
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something is wrong")
+}
+
+function clickEventHandler(){
+    fetch(getTranslationURl(serverURl))
+    .then (response=>response.json())
+    .then (json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
